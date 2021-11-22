dc9a6b81ed8da97e493198b6252e447d09d10580
Clint.Network <me@clint.network>
2021-11-22 13:11:02 +0200

    Add Google Analytics + Hotjar

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,22 @@
 <script>
 export default
 {
-	name: "default"
+	name: "default",
+	mounted() {
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-6VTVXVGNCD');
+
+		(function(h,o,t,j,a,r){
+			h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+			h._hjSettings={hjid:2713172,hjsv:6};
+			a=o.getElementsByTagName('head')[0];
+			r=o.createElement('script');r.async=1;
+			r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+			a.appendChild(r);
+		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+	}
 }
 </script>
 
