a30552abebec1dad2e58afb4e53e8b2fbdf36a77
Clint.Network <me@clint.network>
2021-12-08 00:06:10 +0200

    Little cleanup of roadmap

--- components/Roadmap.vue
+++ components/Roadmap.vue
@@ -6,9 +6,9 @@
 		<p class="mt-5 max-w-prose mx-auto lg:text-xl text-base text-white">
 			Learn more about the Parasol Finance Project.
 		</p>
-		<ul role="list" class="space-y-8 mt-10 lg:mt-14 mx-auto max-w-3xl px-5">
+		<ul class="space-y-8 mt-10 lg:mt-14 mx-auto max-w-3xl px-5" role="list">
 			<li>
-				<a href="#" class="block bg-white rounded-xl shadow-lg hover:bg-gray-50">
+				<a class="block bg-white rounded-xl shadow-lg hover:bg-gray-50" href="#">
 					<div class="px-4 py-4 sm:px-6">
 						<div class="flex items-center justify-between">
 							<p class="text-sm font-medium text-gray-900 truncate">
@@ -27,8 +27,8 @@
 								</p>
 							</div>
 							<div class="mt-2 flex items-center justify-center text-sm text-gray-500 sm:mt-0">
-								<svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-									<path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+								<svg aria-hidden="true" class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+									<path clip-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" fill-rule="evenodd" />
 								</svg>
 								<p>
 									<time datetime="2020-01-07">December 1, 2021</time>
@@ -40,7 +40,7 @@
 			</li>
 
 			<li>
-				<a href="#" class="block bg-white rounded-xl shadow-lg hover:bg-gray-50">
+				<a class="block bg-white rounded-xl shadow-lg hover:bg-gray-50" href="#">
 					<div class="px-4 py-4 sm:px-6">
 						<div class="flex items-center justify-between">
 							<p class="text-sm font-medium text-gray-900 truncate">
@@ -59,8 +59,8 @@
 								</p>
 							</div>
 							<div class="mt-2 flex items-center justify-center text-sm text-gray-500 sm:mt-0">
-								<svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-									<path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+								<svg aria-hidden="true" class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+									<path clip-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" fill-rule="evenodd" />
 								</svg>
 								<p>
 									<time datetime="2020-01-07">December 12, 2021</time>
@@ -72,39 +72,7 @@
 			</li>
 
 			<li>
-				<a href="#" class="block bg-white rounded-xl shadow-lg hover:bg-gray-50">
-					<div class="px-4 py-4 sm:px-6">
-						<div class="flex items-center justify-between">
-							<p class="text-sm font-medium text-gray-900 truncate">
-								Launching Parasol Finance Application
-							</p>
-							<div class="ml-2 flex-shrink-0 flex">
-								<p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-green-800">
-									Planned
-								</p>
-							</div>
-						</div>
-						<div class="mt-2 sm:flex sm:justify-between">
-							<div class="sm:flex">
-								<p class="flex items-center text-sm text-gray-500">
-									The application will propose to list projects, to vote and other stuffs.
-								</p>
-							</div>
-							<div class="mt-2 flex items-center justify-center text-sm text-gray-500 sm:mt-0">
-								<svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-									<path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-								</svg>
-								<p>
-									<time datetime="2020-01-07">January 7, 2022</time>
-								</p>
-							</div>
-						</div>
-					</div>
-				</a>
-			</li>
-
-			<li>
-				<a href="#" class="block bg-white rounded-xl shadow-lg hover:bg-gray-50">
+				<a class="block bg-white rounded-xl shadow-lg hover:bg-gray-50" href="#">
 					<div class="px-4 py-4 sm:px-6">
 						<div class="flex items-center justify-between">
 							<p class="text-sm font-medium text-gray-900 truncate">
@@ -123,11 +91,43 @@
 								</p>
 							</div>
 							<div class="mt-2 flex items-center justify-center text-sm text-gray-500 sm:mt-0">
-								<svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-									<path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+								<svg aria-hidden="true" class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+									<path clip-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" fill-rule="evenodd" />
 								</svg>
 								<p>
-									<time datetime="2020-01-14">January 31, 2022</time>
+									<time datetime="2020-01-14">January 7, 2022</time>
+								</p>
+							</div>
+						</div>
+					</div>
+				</a>
+			</li>
+
+			<li>
+				<a class="block bg-white rounded-xl shadow-lg hover:bg-gray-50" href="#">
+					<div class="px-4 py-4 sm:px-6">
+						<div class="flex items-center justify-between">
+							<p class="text-sm font-medium text-gray-900 truncate">
+								Launching Parasol Finance Application
+							</p>
+							<div class="ml-2 flex-shrink-0 flex">
+								<p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-green-800">
+									Planned
+								</p>
+							</div>
+						</div>
+						<div class="mt-2 sm:flex sm:justify-between">
+							<div class="sm:flex">
+								<p class="flex items-center text-sm text-gray-500">
+									The application will propose to list projects, to vote and other stuffs.
+								</p>
+							</div>
+							<div class="mt-2 flex items-center justify-center text-sm text-gray-500 sm:mt-0">
+								<svg aria-hidden="true" class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+									<path clip-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" fill-rule="evenodd" />
+								</svg>
+								<p>
+									<time datetime="2020-01-07">January 7, 2022</time>
 								</p>
 							</div>
 						</div>
