a982f47c9c865eddb47337b5d840e7c14d943e35
Clint.Network <me@clint.network>
2021-12-12 17:43:10 +0200

    Update Roadmap.vue

--- components/Roadmap.vue
+++ components/Roadmap.vue
@@ -127,7 +127,7 @@
 									<path clip-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" fill-rule="evenodd" />
 								</svg>
 								<p>
-									<time datetime="2020-01-07">Mars 1, 2022</time>
+									<time datetime="2020-01-07">March 1, 2022</time>
 								</p>
 							</div>
 						</div>
